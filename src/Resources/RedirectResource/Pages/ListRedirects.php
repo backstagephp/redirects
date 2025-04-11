c8041d5123ac40142a9470d1c068eb891171683d
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-04-11 16:29:57 +0200

    Add redirects based on copy-paste

--- src/Resources/RedirectResource/Pages/ListRedirects.php
+++ src/Resources/RedirectResource/Pages/ListRedirects.php
@@ -4,8 +4,10 @@ namespace Backstage\Redirects\Filament\Resources\RedirectResource\Pages;
 
 use Backstage\Redirects\Filament\Imports\RedirectImporter;
 use Backstage\Redirects\Filament\Resources\RedirectResource;
+use Backstage\Redirects\Laravel\Models\Redirect;
 use Filament\Actions;
 use Filament\Actions\ActionGroup;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
@@ -50,7 +52,35 @@ class ListRedirects extends ListRecords
                                 ->required()
                                 ->rows(30),
                         ]),
-                    ]),
+                    ])
+                    ->action(function($data) {
+                        $sourceLines = preg_split('/\r\n|\r|\n/', $data['sources']);
+                        $destinationLines = preg_split('/\r\n|\r|\n/', $data['destinations']);
+                        
+                        $redirects = [];
+                        
+                        foreach ($sourceLines as $index => $source) {
+                            $destination = $destinationLines[$index] ?? null;
+                        
+                            if ($destination !== null) {
+                                $redirects[] = [
+                                    'code' => $data['code'],
+                                    'source' => trim($source),
+                                    'destination' => trim($destination),
+                                ];
+                            }
+                        }
+
+                        foreach ($redirects as $redirect) {
+                            Redirect::updateOrCreate([
+                                'source' => $redirect['source'],
+                                'site_id' => Filament::getTenant()->ulid,
+                            ], [
+                                'destination' => $redirect['destination'],
+                                'code' => $redirect['code'],
+                            ]);
+                        }
+                    }),
                 Actions\ImportAction::make('import-file')
                     ->label(__('Import by file'))
                     ->icon('heroicon-o-arrow-up-on-square-stack')
